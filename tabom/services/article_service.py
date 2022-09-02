ee5a079435c65dea628466d74a472d916dc7ff3a
imjoo0 <im_joo0@naver.com>
2022-09-02 18:46:12 +0900

    like test

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,13 +1,23 @@
 from django.core.paginator import Page, Paginator
-from django.db.models import QuerySet
-from tabom.models import Article
+from django.db.models import Prefetch, QuerySet
+
+from tabom.models import Article, Like
 
 
-def get_an_article(article_id: int) -> Article:
-    return Article.objects.get(id=article_id)
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
 
-def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
-    return Article.objects.order_by("-id")[offset : offset + limit]
+
+def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
+    return (
+        Article.objects.order_by("-id")
+        .prefetch_related("like_set")
+        .prefetch_related(Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes"))[
+            offset : offset + limit
+        ]
+    )
 
 def get_article_page(page: int, limit: int) -> Page[Article]:
     return Paginator(Article.objects.order_by("-id"), limit).page(page)
